cef168bc3fa6d0bf2f583d27dc5a18d4f356aa3a
Eddie Kao <eddie@5xcampus.com>
2024-08-05 15:12:18 +0800

    add resumes

---
+++ resumes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Resume(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    introduce = models.CharField(max_length=200)
+    profile = models.TextField()
+    online = models.BooleanField(default=False)
