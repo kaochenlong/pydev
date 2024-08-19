9750b56ce75ad813faa1a8dfc951aab4d7ef74ee
Eddie Kao <eddie@5xcampus.com>
2024-08-19 11:45:47 +0800

    feat: get nonce from braintree

--- src/scripts/components/braintree.js
+++ src/scripts/components/braintree.js
@@ -2,17 +2,51 @@ import Alpine from "alpinejs"
 import dropin from "braintree-web-drop-in"
 
 Alpine.data("braintree_drop_in", () => ({
-  init: function () {
+  init() {
     const token = this.$el.dataset.token
-    const dropin_obj = { container: this.$el, authorization: token }
 
-    dropin
-      .create(dropin_obj)
-      .then((_instance) => {
-        console.log("ok!")
-      })
-      .catch((err) => {
-        console.log(err)
-      })
+    if (token) {
+      dropin
+        .create({ container: this.$el, authorization: token })
+        .then((instance) => {
+          const form = this.$el.closest("form")
+
+          if (form) {
+            form.addEventListener("submit", (e) => {
+              e.preventDefault()
+
+              instance
+                .requestPaymentMethod()
+                .then(({ nonce }) => {
+                  if (nonce) {
+                    this.createNonceField(nonce)
+
+                    // form.submit()
+                  }
+                })
+                .catch((err) => {
+                  console.log("error", err)
+                })
+            })
+          } else {
+            console.log("Form is missing")
+          }
+
+          console.log("ok!")
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    }
+  },
+
+  createNonceField(nonce) {
+    const field = document.createElement("input")
+    field.setAttribute("type", "hidden")
+    field.setAttribute("name", "nonce")
+    field.setAttribute("value", nonce)
+
+    const form = this.$el.closest("form")
+    form.appendChild(field)
   },
 }))
