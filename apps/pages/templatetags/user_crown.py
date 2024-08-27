0898171432f9117fcb68e331b28ac8b17f55ef25
Eddie Kao <eddie@5xcampus.com>
2024-08-27 16:52:23 +0800

    fix: comment has no user

--- apps/pages/templatetags/user_crown.py
+++ apps/pages/templatetags/user_crown.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.filter(name="user_crown")
 def user_crown(user):
-    if user.has_perm("comments.change_comment"):
+    if user and user.has_perm("comments.change_comment"):
         return mark_safe('<i class="text-yellow-400 fa-solid fa-crown"></i>')
 
     return ""
